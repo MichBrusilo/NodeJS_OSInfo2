6ce9c071670879603889d147d0885cdead69fc4f
Michał Brusiło <mibru@interia.pl>
2018-05-09 19:01:29 +0200

    Update osinfo.js

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -2,6 +2,7 @@ var os = require('os');
 
 function getOSinfo() {
     var type = os.type();
+    var time = require('../modules/time.js');
     if(type === 'Darwin') {
         type = 'OSX';
     } else if(type === 'Windows_NT') {
@@ -9,10 +10,12 @@ function getOSinfo() {
     }
     var release = os.release();
     var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (time.print));
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
