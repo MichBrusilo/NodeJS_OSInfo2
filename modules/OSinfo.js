36acb7673e0400eb47e8ed19e86faa42417307fa
Michał Brusiło <mibru@interia.pl>
2018-05-10 08:18:23 +0200

    Update osinfo.js

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -15,7 +15,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (time.print));
+    console.log('Uptime: ~', (time.timeCounting));
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
